61af071e4491cc4a39a6da1db31142f635c82bec
kpusarmiabsensi <kpusarmi.abseni@gmail.com>
2026-02-28 11:09:01 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "saksi-cache-v1";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["./index.html", "./manifest.json"]);
+    })
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
